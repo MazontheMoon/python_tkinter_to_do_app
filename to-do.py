a170e9732265ca9bc8deab274f4ff87ef9516cc2
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2026-04-01 13:32:30 +0100

    Add Scrollbar to listbox

--- to-do.py
+++ to-do.py
@@ -53,64 +53,72 @@ window = tk.Tk()
 # Set window properties
 window.title("To-Do List")
 window.geometry('640x600+50+50') 
-window.config(bg="DarkSlateBlue")
+window.config(bg = "DarkSlateBlue")
 window.resizable(False, False)
 
-# Define title widget
+# Define title frame and label
 titleFrame = Frame(window)
 titleLabel = Label(titleFrame, 
-		 text="To-Do List",
-                 fg="black",
-                 bg="DarkSlateBlue",
+		 text = "To-Do List",
+                 fg = "black",
+                 bg = "DarkSlateBlue",
 		 font = "Garamond 32 bold italic",
-                 pady=20,
-                 padx=10)
-titleLabel.pack(fill="x")
+                 pady = 20,
+                 padx = 10)
+titleLabel.pack(fill = "x")
 
-# Define task widget
+# Define task frame
 taskFrame = Frame(window, 
-                padx=10,
-                highlightbackground="black", 
-                highlightthickness=2)
+                padx = 10,
+                highlightbackground = "black", 
+                highlightthickness = 2)
 
-# Task input
+# Define task input widget
 nameLabel = Label(taskFrame,
-                  text="Enter Task: ",
+                  text = "Enter Task: ",
                   font = "Tahoma 10",
-                  pady=10)
-nameLabel.grid(row=0, column=0)
+                  pady = 10)
+nameLabel.grid(row = 0, column = 0)
 taskEntry = Entry(taskFrame)
-taskEntry.grid(row=0, column=1, sticky="ew")
-taskFrame.columnconfigure(1, weight=1)
+taskEntry.grid(row = 0, column = 1, sticky = "ew")
+taskFrame.columnconfigure(1, weight = 1)
 
-# Define list frame
-taskListFrame = Frame(window,
-                      height=330,
-                      width=500,
-                      bd=2,
-                      bg="DarkSlateBlue")
-
-# Define current task header widget
-currentLabel = Label(taskListFrame,
-                 text="Current Tasks: ",
-                      font = "Tahoma 12 bold",
-                      bg="DarkSlateBlue",
-                      anchor="w",
-                      pady=10)
+# Define task header widget
+currentFrame = Frame(window)
+currentLabel = Label(currentFrame,
+                     text = "Current Tasks: ",
+                     font = "Tahoma 12 bold",
+                     bg = "DarkSlateBlue",
+                     anchor = "w",
+                     pady = 10)
 currentLabel.pack(fill="x")
+
+# Define task list widget
+taskListFrame = Frame(window,
+                      #height = 300,
+                      #width = 500,
+                      bd = 2,
+                      bg = "DarkSlateBlue")
+#taskListFrame.pack_propagate(False)
 
 # Define task listbox widget
 taskList = Listbox(taskListFrame,
-                   font="Tahoma 10",
-                   bg="grey",
-                   #height=15,
-                   width=300,
-                   cursor="hand2", bd=2)
-taskListFrame.pack_propagate(False)
-taskList.pack(side=LEFT, fill=BOTH, padx=2)
+                   font = "Tahoma 10",
+                   bg = "lightgrey",
+                   height = 17,
+                   width = 81,
+                   cursor = "hand2",
+                   highlightbackground = "black",
+                   highlightthickness = 2)
+taskList.pack(side=LEFT, fill = "both", padx = 2)
 
-# taskList.pack(fill="x")
-
+# Add scrollbar to listbox
+scrollbar = Scrollbar(taskListFrame)
+scrollbar.pack(side=RIGHT,
+               fill = "y",
+               padx = 2)
+taskList.config(yscrollcommand = scrollbar.set)
+scrollbar.config(command = taskList.yview)
 
 # Define overview frame 
 overviewFrame = Frame(window)
@@ -119,33 +127,54 @@ overviewFrame = Frame(window)
 completeLabel = Label(overviewFrame,
                  text="Complete Tasks: ",
                       font = "Tahoma 12 bold",
-                      bg="DarkSlateBlue",
-                      anchor="w",
-                      pady=5)
-completeLabel.pack(fill="x")
+                      bg = "DarkSlateBlue",
+                      anchor = "w",
+                      pady = 5)
+completeLabel.pack(fill = "x")
 
 # Define outstanding label widget
 outstandingLabel = Label(overviewFrame,
                  text="Outstanding Tasks: ",
                       font = "Tahoma 12 bold",
-                      bg="DarkSlateBlue",
-                      anchor="w",
-                      pady=5)
-outstandingLabel.pack(fill="x")
+                      bg = "DarkSlateBlue",
+                      anchor = "w",
+                      pady = 5)
+outstandingLabel.pack(fill = "x")
 
 # Add frames to window
 titleFrame.pack()
-taskFrame.pack(fill="x", padx=20)
-taskListFrame.pack(fill="x", padx=20)
-overviewFrame.pack(fill="x", padx=20, pady=10)
+taskFrame.pack(fill = "x", padx = 20)
+currentFrame.pack(fill = "x", padx = 20)
+taskListFrame.pack(fill = "x", padx = 20)
+overviewFrame.pack(fill = "x", padx = 20, pady = 10)
 
 # Buttons - static placement
-btnGuide = Button(window, text="User Guide", padx=10, command=fGuide).place(x=22, y=55)
-btnAdd = Button(window, text="Add Task" , padx=10, command=fAdd).place(x=540, y=145)
-btnDelete = Button(window, text="Delete Task", padx=10).place(x=25, y=560)
-btnComplete = Button(window, text="Complete Task", padx=10).place(x=125, y=560)
-btnOutstanding = Button(window, text="Outstanding Tasks", padx=10).place(x=245, y=560)
-btnExit = Button(window, text="Exit", padx=10, command=fExit).place(x=570, y=560)
+btnGuide = Button(window,
+                  text = "User Guide",
+                  padx = 10,
+                  command = fGuide).place(x = 22, y = 55)
+
+btnAdd = Button(window,
+                text = "Add Task" ,
+                padx = 10,
+                command = fAdd).place(x=540, y=145)
+
+btnDelete = Button(window,
+                   text = "Delete Task",
+                   padx = 10).place(x = 25, y = 560)
+
+btnComplete = Button(window,
+                     text = "Complete Task",
+                     padx = 10).place(x = 125, y = 560)
+
+btnOutstanding = Button(window,
+                        text = "Outstanding Tasks",
+                        padx = 10).place(x = 245, y = 560)
+
+btnExit = Button(window,
+                 text = "Exit",
+                 padx = 10,
+                 command = fExit).place(x = 570, y = 560)
 
 # Display application window
 window.mainloop()
