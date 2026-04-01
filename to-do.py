45c178026e4117fe6e1263101f97c073f451df88
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2026-04-01 12:42:30 +0100

    Modify user guide and improve UI layout - add Listbox
    Updated user guide text and adjusted UI element properties for better layout.

--- to-do.py
+++ to-do.py
@@ -2,7 +2,7 @@
 SD-GAL-05 SD-TA-009 Exercise 001
 Author: Mary Ronan
 Last Modified:
-30/03/2026
+01/04/2026
 A To Do Application in Python built using Tkinter
 '''
 
@@ -17,9 +17,9 @@ from tkinter import messagebox
 
 # Display user guide
 def fGuide():
-        guideText = """How to use the To Do application.
-Enter the task and press the Add Task button to add it to the list.
-When a task has been completed, select the item in the list and mark it as complete by pressing the Complete button.
+        guideText = """How to use the To Do application.\n
+Enter the task and press the Add Task button to add it to the list.\n
+When a task has been completed, select the item in the list and mark it as complete by pressing the Complete button.\n
 To delete a saved task, select the item in the list and delete it by pressing the Delete button."""
         messagebox.showinfo("To-Do: A User Guide", guideText)
 
@@ -53,85 +53,91 @@ titleLabel = Label(titleFrame,
                  padx=10)
 titleLabel.pack(fill="x")
 
-
 # Define task widget
 taskFrame = Frame(window, 
                 padx=10,
                 highlightbackground="black", 
-                highlightthickness=3)
+                highlightthickness=2)
 
 # Task input
 nameLabel = Label(taskFrame,
                   text="Enter Task: ",
-                  font = "Tahoma 12",
+                  font = "Tahoma 10",
                   pady=10)
 nameLabel.grid(row=0, column=0)
 taskName = Entry(taskFrame)
 taskName.grid(row=0, column=1, sticky="ew")
 taskFrame.columnconfigure(1, weight=1)
 
-# Define list widget
+# Define task header widget
+currentFrame = Frame(window,
+                      bg="DarkSlateBlue",
+                     bd=2)
+
+
+# Define list frame
 taskListFrame = Frame(window,
-                      height=280,
+                      height=330,
+                      width=500,
+                      bd=2,
                       bg="DarkSlateBlue")
-taskListLabel = Label(taskListFrame,
+
+# Define current task header widget
+currentLabel = Label(taskListFrame,
                  text="Current Tasks: ",
                       font = "Tahoma 12 bold",
                       bg="DarkSlateBlue",
                       anchor="w",
-                      pady=10,
-                      padx=10)
+                      pady=10)
+currentLabel.pack(fill="x")
+
+# Define task listbox widget
+taskList = Listbox(taskListFrame,
+                   font="Tahoma 10",
+                   bg="grey",
+                   #height=15,
+                   width=300,
+                   cursor="hand2", bd=2)
 taskListFrame.pack_propagate(False)
-taskListLabel.pack(fill="x")
+taskList.pack(side=LEFT, fill=BOTH, padx=2)
+
+#taskList.pack(fill="x")
 
 
-# Define list widget
+# Define overview frame 
 overviewFrame = Frame(window)
+
+# Define complete label widget
 completeLabel = Label(overviewFrame,
                  text="Complete Tasks: ",
                       font = "Tahoma 12 bold",
                       bg="DarkSlateBlue",
                       anchor="w",
-                      pady=10,
-                      padx=10)
+                      pady=5)
 completeLabel.pack(fill="x")
 
-
+# Define outstanding label widget
 outstandingLabel = Label(overviewFrame,
                  text="Outstanding Tasks: ",
                       font = "Tahoma 12 bold",
                       bg="DarkSlateBlue",
                       anchor="w",
-                      pady=10,
-                      padx=10)
+                      pady=5)
 outstandingLabel.pack(fill="x")
 
-
-# Place frames on screen
-
+# Add frames to window
 titleFrame.pack()
 taskFrame.pack(fill="x", padx=20)
-taskListFrame.pack(fill="x", padx=20, pady=20)
-overviewFrame.pack(fill="x", padx=20)
+taskListFrame.pack(fill="x", padx=20)
+overviewFrame.pack(fill="x", padx=20, pady=10)
 
-
-# User guide button
+# Buttons - static placement
 btnGuide = Button(window, text="User Guide", padx=10, command=fGuide).place(x=22, y=55)
-
-# Add button
-btnAdd = Button(window, text="Add Task" , padx=10).place(x=540, y=150)
-
-# Delete button
+btnAdd = Button(window, text="Add Task" , padx=10).place(x=540, y=145)
 btnDelete = Button(window, text="Delete Task", padx=10).place(x=25, y=560)
-
-# Complete button
 btnComplete = Button(window, text="Complete Task", padx=10).place(x=125, y=560)
-
-# Outstanding button
 btnOutstanding = Button(window, text="Outstanding Tasks", padx=10).place(x=245, y=560)
-
-# Exit button
 btnExit = Button(window, text="Exit", padx=10, command=fExit).place(x=570, y=560)
 
-# display window
+# Display application window
 window.mainloop()
